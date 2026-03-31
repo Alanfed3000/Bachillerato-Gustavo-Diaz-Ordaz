29ea87d19f517aa24735501991cf09b06dac2997
Alanfred3000 <josealfredom502@gmail.com>
2026-03-31 13:51:52 -0600

    Subir Docentes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocentesController
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'API funcionando correctamente'
+    ]);
 });
+
+Route::apiResource('docentes', DocentesController::class);
+

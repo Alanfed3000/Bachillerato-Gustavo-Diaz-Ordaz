8276c10ed835d88a4e2d07106eb9c8dd99e48a4b
Alanfred3000 <josealfredom502@gmail.com>
2026-04-02 10:39:06 -0600

    Corrigiendo rutas API

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DocentesController
-
+use App\Http\Controllers\Api\DocentesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
